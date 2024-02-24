ef329f5aa0ebd89a61cef36fe17574c7bc5e7cbc
igmalyshev <ilia.malishev@gmail.com>
2024-02-24 19:07:46 +0300

    Client reconnect to new leader fixed

--- raft/delay.go
+++
@@ -1,9 +0,0 @@
-package nw
-
-import "raft/src/raftApi"
-
-type Delay struct {
-	delay         int64 // delay in microseconds
-	InputChannel  chan raftApi.MsgEvent
-	OutputChannel chan raftApi.MsgEvent
-}
